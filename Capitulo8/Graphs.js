dbc8c39fe9e7917e5dbe6768826e4467138f46b6
montoyitadevelp <simon1018226487@gmail.com>
2023-12-27 07:26:02 -0500

    feat: Algorithm BFS and DFS

--- Capitulo8/Graphs.js
+++ Capitulo8/Graphs.js
@@ -2,7 +2,17 @@ import Queue from '../Capitulo2/Queus/Queues.js';
 import Dictionary from '../Capitulo5/Dictionary/Dictionary.js';
 import Stack from '../Capitulo1/Stack/Stack.js';
 
+let adyacentMatrix = [
+  [0, 2, 4, 0, 0, 0],
+  [2, 0, 2, 4, 2, 0],
+  [4, 2, 0, 0, 3, 0],
+  [0, 4, 0, 0, 3, 2],
+  [0, 2, 3, 3, 0, 2],
+  [0, 0, 0, 2, 2, 0],
+];
+
 function Graph() {
+  this.adyacentMatrix = adyacentMatrix;
   let vertices = []; //{1}
   let adjList = new Dictionary(); //{2}
 
@@ -78,31 +88,201 @@ function Graph() {
   };
 
   //Busqueda profunda
-  this.dfs = function (callback) {
-    let color = initializeColor(); //{1}
+  let time = 0;
+  this.dfs = function () {
+    let color = initializeColor(), //{1}
+      d = [],
+      f = [],
+      p = [];
+    time = 0;
+
+    for (let i = 0; i < vertices.length; i++) {
+      //{3}
+      f[vertices[i]] = 0;
+      d[vertices[i]] = 0;
+      p[vertices[i]] = null;
+    }
     for (let i = 0; i < vertices.length; i++) {
       //{2}
       if (color[vertices[i]] === 'white') {
         //{3}
-        dfsVisit(vertices[i], color, callback); //{4}
+        dfsVisit(vertices[i], color, d, f, p); //{4}
       }
     }
+
+    return {
+      discovered: d,
+      finished: f,
+      predecessors: p,
+    };
   };
-  let dfsVisit = function (u, color, callback) {
-    color[u] = 'grey'; //{5}
-    if (callback) {
-      callback(u);
-    }
-    let neighbors = adjList.get(u); //{7}
+  //Funcion recursiva para la busqueda profunda
+  let dfsVisit = function (u, color, d, f, p) {
+    console.log('discovered ' + u);
+    color[u] = 'grey';
+    d[u] = ++time; //{5}
+    let neighbors = adjList.get(u);
     for (let i = 0; i < neighbors.length; i++) {
-      //{8}
-      let w = neighbors[i]; //{9}
+      let w = neighbors[i];
       if (color[w] === 'white') {
-        //{10}
-        dfsVisit(w, color, callback); //{11}
+        p[w] = u; //{6}
+        dfsVisit(w, color, d, f, p);
       }
     }
-    color[u] = 'black'; //{12}
+    color[u] = 'black';
+    f[u] = ++time; //{7}
+    console.log('explored ' + u);
+  };
+  //Busqueda mas corta entre rutas
+  this.dijkstra = function (src) {
+    let dist = [],
+      visited = [],
+      length = this.adyacentMatrix.length;
+    for (let i = 0; i < length; i++) {
+      //{1}
+      dist[i] = Infinity;
+      visited[i] = false;
+    }
+    dist[src] = 0; //{2}
+    for (let i = 0; i < length - 1; i++) {
+      //{3}
+      let u = minDistance(dist, visited); //{4}
+      visited[u] = true; //{5}
+      for (let v = 0; v < length; v++) {
+        if (
+          !visited[v] &&
+          this.adyacentMatrix[u][v] != 0 &&
+          dist[u] != Infinity &&
+          dist[u] + this.adyacentMatrix[u][v] < dist[v]
+        ) {
+          //{6}
+          dist[v] = dist[u] + this.adyacentMatrix[u][v]; //{7}
+        }
+      }
+    }
+    return dist;
+  };
+
+  let minDistance = function (dist, visited) {
+    let min = Infinity,
+      minIndex = -1;
+
+    for (let v = 0; v < dist.length; v++) {
+      if (visited[v] == false && dist[v] <= min) {
+        min = dist[v];
+        minIndex = v;
+      }
+    }
+    return minIndex;
+  };
+
+  this.floydWarshall = function () {
+    let dist = [],
+      length = this.adyacentMatrix.length,
+      i,
+      j,
+      k;
+    for (i = 0; i < length; i++) {
+      //{1}
+      dist[i] = [];
+      for (j = 0; j < length; j++) {
+        dist[i][j] = this.adyacentMatrix[i][j];
+      }
+    }
+    for (k = 0; k < length; k++) {
+      //{2}
+      for (i = 0; i < length; i++) {
+        for (j = 0; j < length; j++) {
+          if (dist[i][k] + dist[k][j] < dist[i][j]) {
+            dist[i][j] = dist[i][k] + dist[k][j];
+          }
+        }
+      }
+    }
+    return dist;
+  };
+
+  //MST - Minimun Spanning tree
+  this.prim = function () {
+    let parent = [],
+      key = [],
+      visited = [],
+      i;
+    length = this.adyacentMatrix.length;
+    for (i = 0; i < length; i++) {
+      //{1}
+      key[i] = Number.MAX_SAFE_INTEGER;
+      visited[i] = false;
+    }
+    key[0] = 0; //{1}
+    parent[0] = -1;
+    for (i = 0; i < length - 1; i++) {
+      //{3}
+      let u = minDistance(key, visited); //{4}
+      visited[u] = true; //{5}
+      for (let v = 0; v < length; v++) {
+        if (
+          this.adyacentMatrix[u][v] &&
+          visited[v] == false &&
+          this.adyacentMatrix[u][v] < key[v]
+        ) {
+          //{6}
+          parent[v] = u; //{7}
+          key[v] = this.adyacentMatrix[u][v]; //{8}
+        }
+      }
+    }
+    return parent;
+  };
+  let find = function (i, parent) {
+    while (parent[i]) {
+      i = parent[i];
+    }
+    return i;
+  };
+
+  let union = function (i, j, parent) {
+    if (i != j) {
+      parent[j] = i;
+      return true;
+    }
+    return false;
+  };
+  
+  this.kruskal = function () {
+    let length = this.adyacentMatrix.length,
+      parent = [],
+      cost,
+      ne = 0,
+      a,
+      b,
+      u,
+      v,
+      i,
+      j,
+      min;
+    cost = initializeColor(); //{1}
+    while (ne < length - 1) {
+      //{2}
+      for (i = 0, min = Number.MAX_SAFE_INTEGER; i < length; i++) {
+        //{3}
+        for (j = 0; j < length; j++) {
+          if (cost[i][j] < min) {
+            min = cost[i][j];
+            u = i;
+            v = j;
+          }
+        }
+      }
+      u = find(u, parent); //{4}
+      v = find(v, parent); //{5}
+      if (union(u, v, parent)) {
+        //{6}
+        ne++;
+      }
+      cost[u][v] = cost[v][u] = Number.MAX_SAFE_INTEGER;
+    }
+    return parent;
   };
 }
 
@@ -150,4 +330,22 @@ for (let i = 1; i < myVertices.length; i++) {
   }
   console.log(s); //{19}
 }
-graph.dfs(printNode)
+graph.dfs(printNode);
+graph = new Graph();
+myVertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+for (let i = 0; i < myVertices.length; i++) {
+  graph.addVertex(myVertices[i]);
+}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'D');
+graph.addEdge('B', 'E');
+graph.addEdge('C', 'F');
+graph.addEdge('F', 'E');
+let result = graph.dfs();
+
+console.log(result);
+console.log(graph.dijkstra(0));
+console.log(graph.floydWarshall());
+console.log(graph.prim());
+console.log(graph.kruskal())
