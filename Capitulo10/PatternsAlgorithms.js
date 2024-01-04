2ea8e3dceaa7331e83457c7cee3706f11be3c80f
montoyitadevelp <simon1018226487@gmail.com>
2024-01-04 06:56:13 -0500

    feat: fibonacci sequence

--- Capitulo10/PatternsAlgorithms.js
+++ Capitulo10/PatternsAlgorithms.js
@@ -20,3 +20,24 @@ try {
 } catch (ex) {
   alert('i = ' + i + ' error: ' + ex);
 }
+function fibonacci(num) {
+  if (num === 1 || num === 2) {
+    //{1}
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function fib(num) {
+  let n1 = 1,
+    n2 = 1,
+    n = 1;
+  for (let i = 3; i <= num; i++) {
+    n = n1 + n2;
+    n1 = n2;
+    n2 = n;
+  }
+  return n;
+}
+
+console.log(fib(3));
