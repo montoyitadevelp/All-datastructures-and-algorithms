40ee4aa9c460c594bc714593d5d9599ae0832d2a
montoyitadevelp <simon1018226487@gmail.com>
2024-01-05 07:03:54 -0500

    feat: Dynamic programming + MinCoinChange algorithm

--- Capitulo10/PatternsAlgorithms.js
+++ Capitulo10/PatternsAlgorithms.js
@@ -41,3 +41,42 @@ function fib(num) {
 }
 
 console.log(fib(3));
+
+function MinCoinChange(coinsAmount) {
+  let coins = coinsAmount; //{1}
+  let cache = {}; //{2}
+  this.makeChange = function (amount) {
+    let me = this;
+    if (!amount) {
+      //{3}
+      return [];
+    }
+    if (cache[amount]) {
+      //{4}
+      return cache[amount];
+    }
+    let min = [],
+      newMin,
+      newAmount;
+    for (let i = 0; i < coins.length; i++) {
+      //{5}
+      let coin = coins[i];
+      newAmount = amount - coin; //{6}
+      if (newAmount >= 0) {
+        newMin = me.makeChange(newAmount); //{7}
+      }
+      if (
+        newAmount >= 0 && //{8}
+        (newMin.length < min.length - 1 || !min.length) && //{9}
+        (newMin.length || !newAmount) //{10})
+      ) {
+        min = [coin].concat(newMin); //{11}
+        console.log('new Min ' + min + ' for ' + amount);
+      }
+    }
+    return (cache[amount] = min); //{12}
+  };
+}
+
+let minCoinChange = new MinCoinChange([1, 5, 10, 25]);
+console.log(minCoinChange.makeChange(36))
