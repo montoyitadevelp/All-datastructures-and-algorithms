977fe70ea1a3dfe00e4ff542cb207d4d29567c4a
montoyitadevelp <simon1018226487@gmail.com>
2024-01-06 07:34:02 -0500

    feat: Algorithm of knapsack

--- Capitulo10/PatternsAlgorithms.js
+++ Capitulo10/PatternsAlgorithms.js
@@ -79,4 +79,64 @@ function MinCoinChange(coinsAmount) {
 }
 
 let minCoinChange = new MinCoinChange([1, 5, 10, 25]);
-console.log(minCoinChange.makeChange(36))
+console.log(minCoinChange.makeChange(36));
+
+function knapSack(capacity, weights, values, n) {
+  let i,
+    w,
+    a,
+    b,
+    kS = [];
+  for (i = 0; i <= n; i++) {
+    //{1}
+    kS[i] = [];
+  }
+  for (i = 0; i <= n; i++) {
+    for (w = 0; w <= capacity; w++) {
+      if (i == 0 || w == 0) {
+        //{2}
+        kS[i][w] = 0;
+      } else if (weights[i - 1] <= w) {
+        //{3}
+        a = values[i - 1] + kS[i - 1][w - weights[i - 1]];
+        b = kS[i - 1][w];
+        kS[i][w] = a > b ? a : b; //{4} max(a,b)
+      } else {
+        kS[i][w] = kS[i - 1][w]; //{5}
+      }
+    }
+  }
+  findValues(n, capacity, kS, weights, values)
+  return kS[n][capacity]; //{6}
+}
+
+function findValues(n, capacity, kS, weights, values) {
+  let i = n,
+    k = capacity;
+  console.log('Items that are part of the solution:');
+  while (i > 0 && k > 0) {
+    if (kS[i][k] !== kS[i - 1][k]) {
+      console.log(
+        'item ' +
+          i +
+          ' can be part of solution w,v: ' +
+          weights[i - 1] +
+          ',' +
+          values[i - 1]
+      );
+      i--;
+      k = k - kS[i][k];
+    } else {
+      i--;
+    }
+  }
+}
+
+let values = [3, 4, 5],
+  weights = [2, 3, 4],
+  capacity = 5,
+  n = values.length;
+console.log(knapSack(capacity, weights, values, n));
+
+
+
